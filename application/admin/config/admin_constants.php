4147425c6a1cd1ae804837cd69b0dd61166db52b
Hashna <hashan@iran.ir>
2019-03-19 11:44:28 +0330

    Add controller_namespace for web/admin

--- application/admin/config/admin_constants.php
+++ application/admin/config/admin_constants.php
@@ -1,5 +1,6 @@
 <?php
 
 define("CONTROLLER_PATH", ADMIN_PATH . DS . 'controllers');
+define("CONTROLLER_NAMESPACE", "App\\Admin\\Controller");
 define("VIEW_PATH", ADMIN_PATH . DS . 'views');
 define("CONFIG_PATH", ADMIN_PATH . DS . 'config');
--- application/web/config/web_constants.php
+++ application/web/config/web_constants.php
@@ -1,5 +1,6 @@
 <?php
 
 define("CONTROLLER_PATH", WEB_PATH . DS . 'controllers');
+define("CONTROLLER_NAMESPACE", "App\\Web\\Controller");
 define("VIEW_PATH", WEB_PATH . DS . 'views');
 define("CONFIG_PATH", WEB_PATH . DS . 'config');
