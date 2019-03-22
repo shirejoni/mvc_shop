bc8e1dd84cadae8b67daa518cd2ca9a590d4fb78
Hashna <hashan@iran.ir>
2019-03-22 08:36:33 +0330

    Add Validate class for validation data

---
+++ application/lib/Validate.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\lib;
+
+
+class Validate
+{
+    public static function emailValid($email) {
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+
+    public static function passwordValid($password) {
+        return preg_match('/^[a-zA-Z0-9!@\#\$%&\*\-_\.]{8,64}$/', $password);
+    }
+
+}
