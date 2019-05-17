6ac68a350f77d0485c804207d8b216b70ef5e181
shirejoni <hosseinshirjoni@chmail.ir>
2019-05-17 16:49:09 +0330

    0015 - Customer Panel - index

--- application/web/controllers/user.php
+++ application/web/controllers/user.php
@@ -2,12 +2,22 @@
 
 namespace App\Web\Controller;
 
+use App\lib\Request;
+use App\lib\Response;
 use App\system\Controller;
 
+/**
+ * @property Response Response
+ * @property Request Request
+ */
 class ControllerUser extends Controller {
 
     public function index() {
-
+        $this->Response->setOutPut($this->render('user/index'));
     }
 
+
+    public function edit() {
+        //
+    }
 }
